400888fbfbc45de6117709511f548dd29a545761
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-24 12:44:42 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBZjplt35q5pu32ODu5O3B69IxWwhg2LrY",
+    authDomain: "angtodoapp-46992.firebaseapp.com",
+    databaseURL: "https://angtodoapp-46992.firebaseio.com",
+    projectId: "angtodoapp-46992",
+    storageBucket: "angtodoapp-46992.appspot.com",
+    messagingSenderId: "926808264950",
+    appId: "1:926808264950:web:91e4c954bea9cc9172dcd7",
+    measurementId: "G-JJJKHE06LC",
+  },
 };
 
 /*
