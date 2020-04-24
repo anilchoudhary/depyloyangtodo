315b2927075ba20eb094c9e39eedd3ca1243c216
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-24 12:57:48 +0530

    updated enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBZjplt35q5pu32ODu5O3B69IxWwhg2LrY",
+    authDomain: "angtodoapp-46992.firebaseapp.com",
+    databaseURL: "https://angtodoapp-46992.firebaseio.com",
+    projectId: "angtodoapp-46992",
+    storageBucket: "angtodoapp-46992.appspot.com",
+    messagingSenderId: "926808264950",
+    appId: "1:926808264950:web:91e4c954bea9cc9172dcd7",
+    measurementId: "G-JJJKHE06LC",
+  },
 };
